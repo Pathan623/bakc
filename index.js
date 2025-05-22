feaa7d626c58d1dd5a72705d79b61101a06f33b0
Pathan623 <msaadkhanpathan0786@gmail.com>
2025-05-22 10:19:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twi", (req, res) => {
+  res.send("<h1> pleased to meet you </h1>");
+});
+
+app.get("/yout", (req, res) => {
+  res.send("<h4> this is a youtube  </h4>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
